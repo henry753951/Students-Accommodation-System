a3e7ef5bb7ade4c0750350f6c9924e8d40c499f8
henry753951 <henryboy753951@gmail.com>
2024-06-02 05:04:19 +0800

    refactor: Remove unused page a-[id].vue

---
+++ pages/rentals/index.vue
@@ -0,0 +1,46 @@
+
+<template>
+  <div>
+    <div
+      v-for="rental in rentals "
+      :key="rental.id"
+    >
+      <div>{{ rental.name }}</div>
+      <div>{{ rental.address }}</div>
+      <NuxtLink :to="'/rentals/'+rental.id">
+        詳細資訊
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  name: '租屋清單',
+});
+const route = useRoute();
+const id = ref(route.params.id);
+const rentals = ref([
+  {
+    id: 1,
+    name: '房屋1',
+    address: '地址1',
+  },
+  {
+    id: 2,
+    name: '房屋2',
+    address: '地址2',
+  },
+  {
+    id: 3,
+    name: '房屋3',
+    address: '地址3',
+  },
+]);
+
+
+</script>
+
+<style>
+
+</style>
