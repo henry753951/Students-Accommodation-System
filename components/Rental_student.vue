0b719cf990ea0a53be11214ef309435724237d97
印哲 莊 <q0963212511@gmail.com>
2024-06-02 01:25:35 +0800

    wait interview problem

--- components/Rental_student.vue
+++ components/Rental_student.vue
@@ -1,211 +1,45 @@
 <template>
-  <div class="bg-slate-200 h-16 flex items-center  ">
-    <div class="flex items-center justify-between w-full px-4">
-      <div class="flex items-center space-x-4">
-        <NuxtLink to="/">
-          <img src="~/assets/images/House.png" alt="Logo" class="h-8" />
-        </NuxtLink>
-        <h1 class="text-2xl font-bold text-white">Student Rental</h1>
+  <div class="space-y-10">
+    <section>
+      <h2 class="text-xl font-bold mb-4">Property Form</h2>
+      <RentalAddrForm v-model:address="address" v-model:landlordName="landlordName" />
+    </section>
+
+    <section>
+      <h2 class="text-xl font-bold mb-4">Property Form</h2>
+      <RentalPropertyForm 
+      v-model:price="price" 
+      v-model:description="description" 
+      v-model:is_public="isPublic" 
+      v-model:propertyAttributesType="propertyAttributesType" 
+      v-model:propertyAttributesSex="propertyAttributesSex" 
+      v-model:propertyAttributesSubsidy="propertyAttributesSubsidy" 
+      />
+    </section>
+
+    <Button type="submit" @click="handleSubmit">Submit</Button>
+
+    <section>
+      <h2 class="text-xl font-bold mb-4">表單內容統整</h2>
+      <p>addr: {{ address }}</p>
+      <p>landlordname: {{ landlordName }}</p>
+      <p>Price: {{ price }}</p>
+      <p>Description: {{ description }}</p>
+      <p>Is Public: {{ isPublic }}</p>
+      <p>Property Attributes: [{{ propertyAttributesType }},{{ propertyAttributesSex }},{{ propertyAttributesSubsidy }}]</p>
+    </section>
+    <section>
+      <h2 class="text-xl font-bold mb-4">Import Link</h2>
+      <input v-model="idtntity" type="radio" value="teacher"> Teacher
+      <input v-model="idtntity" type="radio" value="student"> Student
+      <div v-if="idtntity === 'student'">
+        <button @click="">Import Link</button>
+        <p>Link: {{ link }}</p>
       </div>
-      <div class="flex items-center space-x-4">
-        <NuxtLink to="/auth/login" class="text-white">
-          Login
-        </NuxtLink>
-        <NuxtLink to="/auth/logout" class="text-white">
-          Logout
-        </NuxtLink>
-        <DropdownMenu>
-          <DropdownMenuTrigger as-child>
-            <img src="~/assets/images/images.png" alt="User" class="rounded-full h-8 w-8" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent class="w-56">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <User class="mr-2 h-4 w-4" />
-                <span>Profile</span>
-                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard class="mr-2 h-4 w-4" />
-                <span>Billing</span>
-                <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Settings class="mr-2 h-4 w-4" />
-                <span>Settings</span>
-                <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Keyboard class="mr-2 h-4 w-4" />
-                <span>Keyboard shortcuts</span>
-                <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Users class="mr-2 h-4 w-4" />
-                <span>Team</span>
-              </DropdownMenuItem>
-              <DropdownMenuSub>
-                <DropdownMenuSubTrigger>
-                  <UserPlus class="mr-2 h-4 w-4" />
-                  <span>Invite users</span>
-                </DropdownMenuSubTrigger>
-                <DropdownMenuPortal>
-                  <DropdownMenuSubContent>
-                    <DropdownMenuItem>
-                      <Mail class="mr-2 h-4 w-4" />
-                      <span>Email</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <MessageSquare class="mr-2 h-4 w-4" />
-                      <span>Message</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                      <PlusCircle class="mr-2 h-4 w-4" />
-                      <span>More...</span>
-                    </DropdownMenuItem>
-                  </DropdownMenuSubContent>
-                </DropdownMenuPortal>
-              </DropdownMenuSub>
-              <DropdownMenuItem>
-                <Plus class="mr-2 h-4 w-4" />
-                <span>New Team</span>
-                <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Github class="mr-2 h-4 w-4" />
-              <span>GitHub</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <LifeBuoy class="mr-2 h-4 w-4" />
-              <span>Support</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem disabled>
-              <Cloud class="mr-2 h-4 w-4" />
-              <span>API</span>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <LogOut class="mr-2 h-4 w-4" />
-              <span>Log out</span>
-              <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    </div>
+    </section>
   </div>
-  <div>
-    <form @submit.prevent="onSubmit" class="space-y-6 border-2 border-black p-4 w-1/2">
-      <div class="flex flex-row">
-        <div class="mr-10 w-1/2">
-          <FormField v-slot="{ componentField }" name="StudentName">
-            <FormItem>
-              <FormLabel>Student Name</FormLabel>
-              <FormControl>
-                <Input type="text" disabled placeholder="莊印哲" v-bind="componentField" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          </FormField>
-        </div>
-        <div class=" w-1/2">
-          <FormField v-slot="{ componentField }" name="StudentID">
-            <FormItem>
-              <FormLabel>Student ID</FormLabel>
-              <FormControl>
-                <Input type="text" disabled placeholder="A1105513" v-bind="componentField" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          </FormField>
-        </div>
-
-
-      </div>
-
-      <div class="flex flex-row">
-        <div class="mr-10 w-1/2">
-          <FormField v-slot="{ componentField }" name="LandlordName">
-            <FormItem>
-              <FormLabel>Landlord Name</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="Enter name" v-bind="componentField" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          </FormField>
-        </div>
-        <div class=" w-1/2">
-          <FormField v-slot="{ componentField }" name="LandlordPhone">
-            <FormItem>
-              <FormLabel>LandlordPhone</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="Enter Phone" v-bind="componentField" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          </FormField>
-        </div>
-
-
-      </div>
-
-      <FormField v-slot="{ componentField }" name="rent">
-        <FormItem>
-          <FormLabel>租屋類型</FormLabel>
-          <Select>
-            <SelectTrigger class="w-[150px]">
-              <SelectValue placeholder="Select Type" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem v-for="message in House_type" :value="message.value">
-                  {{ message.label }}
-                </SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </FormItem>
-      </FormField>
-
-      <FormField v-for="(message, index) in Form_information" :key="index" v-slot="{ componentField }"
-        :name="message.formlabel">
-        <FormItem>
-          <FormLabel>{{ message.formlabel }}</FormLabel>
-          <FormControl>
-            <Input type="text" :placeholder="message.placeholder" v-bind="componentField" />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      </FormField>
-
-
-      <!-- Submit Button -->
-      <Button class="hover:bg-black  hover:text-white" type="submit" variant="outline" @click="() => {
-        if (request_check) {
-          toast({
-            title: 'Request Sent',
-            description: 'Your request has been sent successfully!',
-          });
-        } else {
-          toast({
-            title: 'Request Failed',
-            description: 'Please fill in all the required fields and try again.',
-          });
-        }
-      }">Submit</Button>
-    </form>
-  </div>
-
 </template>
+
 
 <script setup lang="ts">
 import { toTypedSchema } from '@vee-validate/zod';
@@ -214,37 +48,78 @@ import * as z from 'zod';
 import { useToast } from '@/components/ui/toast/use-toast'
 const { toast } = useToast();
 import type { DropdownMenuCheckboxItemProps } from 'radix-vue'
+import type { Database, Tables, Enums } from "~/database.types";
+const supabase = useSupabaseClient<Database>();
+
 
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 
-type Checked = DropdownMenuCheckboxItemProps['checked']
+const address = ref('');
+const landlordName = ref('');
+const price = ref(0);
+const description = ref('');
+const isPublic = ref(false);
+const propertyAttributesType = ref('');
+const propertyAttributesSex = ref('無');
+const propertyAttributesSubsidy = ref(false);
 
-const showStatusBar = ref<Checked>(true)
-const showActivityBar = ref<Checked>(false)
-const showPanel = ref<Checked>(false)
+// 看有那些問題
 
-const username = ref('user name from supabase before login');
+const idtntity = ref("");
+let link = ref("");
+let all_interview = ref({});
+onMounted(async () => {
+  if (idtntity.value == "teacher") {
 
-const House_type = [
-  { value: '整層住家', label: '整層住家' },
-  { value: '獨立套房', label: '獨立套房' },
-  { value: '分租套房', label: '分租套房' },
-  { value: '雅房', label: '雅房' },
-  { value: '大樓', label: '大樓' },]
+  } else if (idtntity.value == "student") {
+    // 直接匯入
+    link.value = await get_student_information(idtntity.value);
+    all_interview.value = await get_all_interview(link.value) ?? {};
+  }
+});
 
-const Form_information = ref([
-  { formlabel: "Address", placeholder: "Enter address" },
-  { formlabel: "Price", placeholder: "Enter Price" },
-  { formlabel: "Description", placeholder: "Other description" },
-]);
+const get_student_information = async (student_id: String) => {
+  const { data, error } = await supabase
+    .from('interview_record')
+    .select("*")
+    .eq('student_id', student_id)
+  if (error) {
+    console.error(error)
+    return
+  }
+  return data[0]["get_interview_link"]
+}
+
+const get_all_interview = async (link: String) => {
+  const { data, error } = await supabase
+    .from('interview_problem')
+    .select("*")
+    .eq('get_interview_link', link)
+  if (error) {
+    console.error(error)
+    return
+  }
+  return data
+}
+
+
+const SubmitToInterview = async (time: string) => {
+  const { data, error } = await supabase
+    .from("interview_problem")
+    .insert([
+      {
+        // all problem
+      }
+    ])
+  if (error) {
+    toast({
+      title: "Error",
+      description: error.message,
+      variant: "destructive",
+    });
+    return 'error';
+  }
+};
 
 const formSchema = toTypedSchema(
   z.object({
@@ -257,10 +132,85 @@ const form = useForm({
   validationSchema: formSchema,
 });
 
-const request_check = ref(false);
-const onSubmit = form.handleSubmit(values => {
-  // console.log('Form submitted!', values);
-});
+
+const SubmitToRentalprop = async (time: string): Promise<string> => {
+  const { data, error } = await supabase
+    .from("rental_property")
+    .insert([
+      {
+        "address": address.value,
+        "landlord_id": 'cc7c9e40-af78-45d5-907f-66bcf550d2ad',
+        "created_at": time,
+        "updated_at": time,
+      }
+    ])
+    .select("*")
+  if (error) {
+    toast({
+      title: "Error",
+      description: error.message,
+      variant: "destructive",
+    });
+    return 'error';
+  }
+  return data[0]['id'];
+};
+
+const SubmitToRentalinfo = async (rental_id: string, time: string) => {
+  console.log("BINHAN SO SMALL");
+  const { data, error } = await supabase
+    .from("rental_property_info")
+    .insert([
+      {
+        "rental_property_id": rental_id,
+        "price": price.value,
+        "is_public": false,
+        "description": description.value,
+        "property_attributes": JSON.stringify({
+          type: propertyAttributesType.value,
+          sex: propertyAttributesSex.value,
+          subsidy: propertyAttributesSubsidy.value
+        }),
+        "created_at": time,
+        "updated_at": time,
+      }
+    ])
+    .select("*");
+  if (error) {
+    toast({
+      title: "Error",
+      description: error.message,
+      variant: "destructive",
+    });
+    return;
+  }
+};
+
+const handleSubmit = async () => {
+  const confirmation = confirm(`
+    Address: ${address.value}
+    Landlord Name: ${landlordName.value}
+    Price: ${price.value}
+    Description: ${description.value}
+    Is Public: ${isPublic.value}
+    Property Attributes: ${propertyAttributesType.value}, ${propertyAttributesSex.value}, ${propertyAttributesSubsidy.value}
+    
+    確定要送出嗎?
+  `);
+
+  if (confirmation) {
+    //提交表單的邏輯
+    //送出到supabase
+    let time = new Date().toISOString()
+    let rental_id = await SubmitToRentalprop(time)
+    SubmitToRentalinfo(rental_id, time)
+    SubmitToInterview(time)
+    console.log(rental_id, time);
+  } else {
+    console.log(confirmation);
+  }
+};
+
 </script>
 
 <style scoped>
