64f97c633dcaa460992b5a1256407c37a6954400
BearYep <92510812+BearYep@users.noreply.github.com>
2024-06-07 20:35:24 +0800

    Update Interview List

---
+++ components/interview/TableRow.vue
@@ -0,0 +1,44 @@
+<template>
+  <TableRow>
+    <TableCell class="font-medium">
+      {{ teacherName }}
+    </TableCell>
+    <TableCell>{{ studentName }}</TableCell>
+    <TableCell>{{ createdTime }}</TableCell>
+    
+    <TableCell>
+      <NuxtLink
+        to="/advertisement"
+      >
+        {{ location }}
+      </NuxtLink>
+    </TableCell>
+    <TableCell class="grid grid-cols-2 g-5">
+      <NuxtLink
+        to="/interview/record/id"
+        class="place-self-center"
+      >
+        <Button>編輯</Button>
+      </NuxtLink>
+      <Button
+        variant="destructive"
+        class="place-self-center"
+      >
+        刪除
+      </Button>
+    </TableCell>
+  </TableRow>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  teacherName: {type: String, required: false, default: '無資料'},
+  studentName: {type: String, required: false, default: '無資料'},
+  createdTime: {type: String, required: false, default: '無資料'},
+  location: {type: String, required: false, default: '無資料'},
+});
+</script>
+
+<style>
+
+</style>
