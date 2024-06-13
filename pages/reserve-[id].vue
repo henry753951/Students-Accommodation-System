ac7747e6f7b5dbb6054360d7a5eaccc3661d7d84
scott <scott5497ify@gmail.com>
2024-06-14 00:55:12 +0800

    feat: reserve page

--- pages/reserve-[id].vue
+++ pages/reserve-[id].vue
@@ -4,8 +4,18 @@
       <h2 class="text-2xl font-bold mb-6 text-center">
         預約表單 - {{ type }}
       </h2>
-      {{ form }}
       <form @submit.prevent="handleSubmit">
+        <div class="mb-4">
+          <Label for="student_id">學生ID</Label>
+          <Input
+            id="student_id"
+            v-model="form.student_id"
+            type="text"
+            class="w-full border border-gray-300 rounded mt-1"
+            readonly
+            required
+          />
+        </div>
         <div class="mb-4">
           <Label for="phone">電話</Label>
           <Input
@@ -36,11 +46,29 @@
           />
         </div>
         <div class="mb-4">
-          <Label for="date">選擇時間</Label>
-          <Calendar
-            v-model="form.date"
-            class="w-full border border-gray-300 rounded mt-1"
-          />
+          <FormField name="date">
+            <FormLabel class="mr-2">選擇日期</FormLabel>
+            <Popover>
+              <PopoverTrigger as-child>
+                <Button
+                  variant="outline"
+                  :class="[
+                    'w-[280px] justify-start text-left font-normal',
+                    !form.date && 'text-muted-foreground'
+                  ]"
+                >
+                  <CalendarIcon class="mr-2 h-4 w-4" />
+                  {{ form.date ? form.date : 'Pick a date' }}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent class="w-auto p-0">
+                <Calendar
+                  v-model="form.date"
+                  initial-focus
+                />
+              </PopoverContent>
+            </Popover>
+          </FormField>
         </div>
         <div class="text-center">
           <Button
@@ -56,15 +84,20 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { Calendar as CalendarIcon } from 'lucide-vue-next';
+
 const route = useRoute();
 const type = ref(route.params.id);
+const user = useUser();
 
 const form = ref({
+  student_id: computed(() => user.value?.id || ''), // 使用 computed 來動態獲取 user.id
   phone: '',
   email: '',
   message: '',
-  date: null,
+  date: undefined as Date | undefined,
 });
 
 const handleSubmit = () => {
