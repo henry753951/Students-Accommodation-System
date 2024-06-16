d01e0c5f17f2366e2af71278824b491ae46bed28
BearYep <92510812+BearYep@users.noreply.github.com>
2024-06-16 19:41:45 +0800

    Update class

--- components/ReservationDrawer.vue
+++ components/ReservationDrawer.vue
@@ -8,20 +8,23 @@
         預約清單
       </Button>
     </DrawerTrigger>
-    <DrawerContent class="w-1/3 h-full absolute left-2/3">
-        <DrawerHeader>
-          <DrawerTitle>預約清單</DrawerTitle>
-          <DrawerDescription>以下是您的預約列表</DrawerDescription>
-        </DrawerHeader>
-        <ScrollArea class="h-full">
-          <ReservationList v-if="user?.roles.includes('student')"/>
-          <ReservationListLandlord v-if="user?.roles.includes('landlord')"/>
-        </ScrollArea>
-        <DrawerClose>
-          <Button variant="outline" class="border-2 border-black">
-            關閉
-          </Button>
-        </DrawerClose>
+    <DrawerContent class="w-1/3 h-full left-2/3">
+      <DrawerHeader>
+        <DrawerTitle>預約清單</DrawerTitle>
+        <DrawerDescription>以下是您的預約列表</DrawerDescription>
+      </DrawerHeader>
+      <ScrollArea class="h-full">
+        <ReservationList v-if="user?.roles.includes('student')" />
+        <ReservationListLandlord v-if="user?.roles.includes('landlord')" />
+      </ScrollArea>
+      <DrawerClose>
+        <Button
+          variant="outline"
+          class="border-2 border-black"
+        >
+          關閉
+        </Button>
+      </DrawerClose>
     </DrawerContent>
   </Drawer>
 </template>
